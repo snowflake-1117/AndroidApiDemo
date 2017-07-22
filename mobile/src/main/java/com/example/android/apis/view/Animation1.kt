8d1c82b308a5b184be3734955e13264b933e6ff5
Hyemin Kim <1117hyemin@gmail.com>
2017-07-22 10:38:28 +0900

    Convert Animation1 Java file into Kotiln file

--- mobile/src/main/java/com/example/android/apis/view/Animation1.kt
+++ mobile/src/main/java/com/example/android/apis/view/Animation1.kt
@@ -14,32 +14,31 @@
  * limitations under the License.
  */
 
-package com.example.android.apis.view;
+package com.example.android.apis.view
 
 // Need the following import to get access to the app resources, since this
 // class is in a sub-package.
-import com.example.android.apis.R;
+import com.example.android.apis.R
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 
-public class Animation1 extends Activity implements View.OnClickListener {
+class Animation1 : Activity(), View.OnClickListener {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.animation_1);
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.animation_1)
 
-        View loginButton = findViewById(R.id.login);
-        loginButton.setOnClickListener(this);
+        val loginButton = findViewById<View>(R.id.login)
+        loginButton.setOnClickListener(this)
     }
 
-    public void onClick(View v) {
-        Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
-        findViewById(R.id.pw).startAnimation(shake);
+    override fun onClick(v: View) {
+        val shake = AnimationUtils.loadAnimation(this, R.anim.shake)
+        findViewById<View>(R.id.pw).startAnimation(shake)
     }
 
 }
