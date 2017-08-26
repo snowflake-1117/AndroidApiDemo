599362a0bd858bc1a4a1366d88cd2bb6bca371cd
Hyemin Kim <1117hyemin@gmail.com>
2017-08-26 17:00:18 +0900

    List2 코틀린으로 변환
    deprecated 함수 삭제 및 로더 대체 적용

---
+++ mobile/src/main/java/com/example/android/apis/view/List2.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.apis.view
+
+import android.app.ListActivity
+import android.app.LoaderManager
+import android.content.CursorLoader
+import android.content.Loader
+import android.database.Cursor
+import android.provider.ContactsContract.Contacts
+import android.os.Bundle
+import android.widget.SimpleCursorAdapter
+
+/**
+ * A list view example where the
+ * data comes from a cursor.
+ */
+class List2 : ListActivity(), LoaderManager.LoaderCallbacks<Cursor> {
+    // Get a cursor with all people
+    private lateinit var mCursor: Cursor
+    private lateinit var mAdapter: SimpleCursorAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mCursor = contentResolver.query(Contacts.CONTENT_URI,
+                CONTACT_PROJECTION, null, null, null)
+
+        mAdapter = SimpleCursorAdapter(this,
+                // Use a template that displays a text view
+                android.R.layout.simple_list_item_1,
+                // Give the cursor to the list adatper
+                mCursor,
+                // Map the NAME column in the people database to...
+                arrayOf(Contacts.DISPLAY_NAME),
+                // The "text1" view defined in the XML template
+                intArrayOf(android.R.id.text1),
+                0)
+
+        listAdapter = mAdapter
+
+        loaderManager.initLoader(0, null, this)
+    }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        return CursorLoader(this@List2, Contacts.CONTENT_URI, CONTACT_PROJECTION, null, null, null)
+    }
+
+    override fun onLoadFinished(loader: Loader<Cursor>?, cursor: Cursor?) {
+        mAdapter.swapCursor(cursor)
+    }
+
+    override fun onLoaderReset(loader: Loader<Cursor>?) {
+        mAdapter.swapCursor(null)
+    }
+
+    companion object {
+        private val CONTACT_PROJECTION = arrayOf(Contacts._ID, Contacts.DISPLAY_NAME)
+    }
+}
